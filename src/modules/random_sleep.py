3ef47a57d05d597f06afaf7637f57569ddb49e25
Lucas WINTZ <lucas.wintz@laposte.net>
2022-08-10 11:21:20 -0400

    Random sleep docstring

--- src/modules/random_sleep.py
+++ src/modules/random_sleep.py
@@ -3,4 +3,13 @@ from random import randint
 
 
 def random_sleep(start: int = 2, end: int = 4) -> int:
+    """Random sleep time.
+
+    Args:
+        start (int, optional): range start. Defaults to 2.
+        end (int, optional): range end. Defaults to 4.
+
+    Returns:
+        int: random int
+    """
     return sleep(randint(start, end))
